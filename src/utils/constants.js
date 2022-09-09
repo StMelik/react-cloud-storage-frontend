546a21508b5f485ab25847b2930830130247106d
Stanislav Meleshchik <st.mele.a@yandex.ru>
2022-09-09 17:09:47 +0300

    fix: исправить адрес сервера

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,2 @@
-
-
-export const SERVER_URL = 'http://localhost:3000'
-// export const SERVER_URL = 'https://cloud-storage23.herokuapp.com'
+export const SERVER_URL = 'https://cloud-storage23.herokuapp.com'
+// export const SERVER_URL = 'http://localhost:3000'
