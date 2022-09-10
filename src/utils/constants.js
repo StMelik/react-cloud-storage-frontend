872b3fb90f4221803e37cd74663d959b4e9d3839
Stanislav Meleshchik <st.mele.a@yandex.ru>
2022-09-10 04:04:14 +0300

    fix: изменить сервер

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,3 @@
-export const SERVER_URL = 'https://cloud-storage23.herokuapp.com'
+export const SERVER_URL = 'https://cloud-storage-api.stmelik.repl.co/'
+// export const SERVER_URL = 'https://cloud-storage23.herokuapp.com'
 // export const SERVER_URL = 'http://localhost:3000'
